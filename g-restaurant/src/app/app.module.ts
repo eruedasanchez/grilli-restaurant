5a63bd60570b73d5c32b16d94c88bee7026391ef
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-08-16 18:22:18 -0300

    Add Header Component

--- g-restaurant/src/app/app.module.ts
+++ g-restaurant/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PreloaderComponent } from './components/preloader/preloader.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreloaderComponent,
-    FooterComponent
+    TopBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
